97eba58fb77868951cc82e38d7292752f4faec7a
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-07 18:46:28 +0530

    functions 01

---
+++ 03functions.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(2);
+function getUpper(val) {
+    return val.toUpperCase();
+}
+getUpper("JSingh");
+function signUp(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+loginUser("j", "j@j.com");
---
+++ 03functions.ts
@@ -0,0 +1,17 @@
+function addTwo(num: number) {
+  return num + 2;
+}
+addTwo(2);
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+getUpper("JSingh");
+
+function signUp(name: string, email: string, isPaid: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
+loginUser("j", "j@j.com");
+// 1:03
+
+export {};
