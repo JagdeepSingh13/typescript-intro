5bf27e4cc27b1f8e4b7cf2b7c1f62bc301d2b9f5
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-08 18:30:31 +0530

    objects 02

--- 04objects.ts
+++ 04objects.ts
@@ -12,3 +12,34 @@ createUser(newUser);
 function createCourse(): { name: string; price: number } {
   return { name: "nextjs", price: 1234 };
 }
+
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function crtUser(user: User): User {
+  return { name: "", email: "", isActive: true };
+}
+
+crtUser({ name: "", email: "", isActive: true });
+
+type stUser = {
+  readonly _id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+  credNumber?: number;
+};
+
+let myUser: stUser = {
+  _id: "123",
+  name: "js",
+  email: "j@j",
+  isActive: true,
+};
+
+// type t3 = t1 & t2 (t -> type we defined earlier)
+
+export {};
